2035af8f475caaf5f2b6fac8564ed98167a7766e
Benjamin Schwendner <schwendner@axinom.com>
2023-03-07 15:00:09 +0100

    feat: enable toast notifications on mosaic-ui

--- services/media/workflows/src/index.tsx
+++ services/media/workflows/src/index.tsx
@@ -1,4 +1,5 @@
 import { PiletApi } from '@axinom/mosaic-portal';
+import { initializeUi } from '@axinom/mosaic-ui';
 import React from 'react';
 import { initializeApolloClient } from './apolloClient/apolloClient';
 import { bindExtensions } from './externals/piralExtensions';
@@ -21,6 +22,9 @@ export const mediaManagementParentName = 'media-management';
 
 export function setup(app: PiletApi): void {
   initializeConfig(app.meta.custom);
+
+  // Provide the PiletAPI to the UI components (e.g. for raising toast notifications)
+  initializeUi(app);
 
   // Initializing the GraphQL clients
   initializeApolloClient(
