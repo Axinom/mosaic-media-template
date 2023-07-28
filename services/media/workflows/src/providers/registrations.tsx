2902f891db034d226f18e156f0bef4b93368dbb2
Inga Eglite <eglite@axinom.com>
2023-07-28 13:51:11 +0200

    chore: enables bulk selection for provider selection stations (movies, episodes)

--- services/media/workflows/src/providers/registrations.tsx
+++ services/media/workflows/src/providers/registrations.tsx
@@ -13,8 +13,7 @@ export function register(app: PiletApi): void {
         kind="SelectionExplorer"
         title="Select Movie"
         stationKey="FASTMovieSelection"
-        allowBulkSelect={false}
-        enableSelectAll={false}
+        allowBulkSelect={true}
         defaultFilterValues={{
           mainVideoId: true,
         }}
@@ -55,8 +54,7 @@ export function register(app: PiletApi): void {
         kind="SelectionExplorer"
         title="Select Episode"
         stationKey="FASTEpisodeSelection"
-        allowBulkSelect={false}
-        enableSelectAll={false}
+        allowBulkSelect={true}
         defaultFilterValues={{
           mainVideoId: true,
         }}
