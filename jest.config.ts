a9eb15d804d98d47aa7cc1d7fa69cc56d6e65420
Sergey Trusov <117624390+AxTrusov@users.noreply.github.com>
2023-08-29 12:35:42 +0300

    [AB#40831] added a workaround to jest memory leak issue for CI PR builds (#138)
    * fix: added a workaround to jest memory leak issue for CI PR builds
    [AB#40831]
    
    * chore: limit the amount of workers for the CI script
    [AB#40831]
    
    * chore: increase tests debug timeout to 15min

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,6 @@ import type { JestConfigWithTsJest } from 'ts-jest';
 
 const jestConfig: JestConfigWithTsJest = {
   preset: 'ts-jest',
-  testTimeout: 60000,
   modulePathIgnorePatterns: ['./dist/'],
   setupFilesAfterEnv: ['jest-expect-message', 'jest-extended/all'],
   projects: [
--- services/vod-to-live/service/jest.config.ts
+++ services/vod-to-live/service/jest.config.ts
@@ -3,9 +3,9 @@ import type { JestConfigWithTsJest } from 'ts-jest';
 const jestConfig: JestConfigWithTsJest = {
   preset: 'ts-jest',
   modulePathIgnorePatterns: ['./dist/', './legacy/'],
-  setupFilesAfterEnv: ['jest-expect-message'],
+  setupFilesAfterEnv: ['jest-expect-message', '<rootDir>/jest.setup.ts'],
   displayName: 'vod-to-live-service',
-  testTimeout: 60000,
+  workerIdleMemoryLimit: '1GB',
 };
 
 export default jestConfig;
