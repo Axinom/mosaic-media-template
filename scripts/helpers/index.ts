efb7e55cb3cf72984c8ae2c2e6a4b7e546ab6633
Sergey Trusov <117624390+AxTrusov@users.noreply.github.com>
2023-08-29 13:19:04 +0300

    [AB#40804] make logical replication slot name empty in .env.template (#139)
    * fix: make logical replication slot name empty in template
    [AB#40804]
    
    * chore: adjust test config for CI PR builds
    [AB#40804]

--- scripts/helpers/index.ts
+++ scripts/helpers/index.ts
@@ -2,3 +2,4 @@ export * from './db-command-helpers';
 export * from './db-test-helpers';
 export * from './service-account-setup';
 export * from './token-utils';
+export * from './update-env-file';
