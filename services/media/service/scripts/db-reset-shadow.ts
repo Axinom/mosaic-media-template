03d4446960b707a9f152878b0ad7899819e0f0af
Sergey Trusov <117624390+AxTrusov@users.noreply.github.com>
2023-09-14 10:42:21 +0300

    feat: ensureReplicationSlotAndPublicationExist helper used (#153)
    [AB#41392]

--- services/media/service/scripts/db-reset-shadow.ts
+++ services/media/service/scripts/db-reset-shadow.ts
@@ -1,31 +1,13 @@
 /* eslint-disable no-console */
-import { getValidatedConfig, pick } from '@axinom/mosaic-service-common';
+import {
+  getBasicDbConfigDefinitions,
+  getValidatedConfig,
+} from '@axinom/mosaic-service-common';
 import { reset } from 'graphile-migrate';
-import { getConfigDefinitions, getMigrationSettings } from '../src/common';
+import { getMigrationSettings } from '../src/common';
 
 async function main(): Promise<void> {
-  const config = getValidatedConfig(
-    pick(
-      getConfigDefinitions(),
-      'pgRootConnectionString',
-      'dbOwnerConnectionString',
-      'dbShadowConnectionString',
-      'dbLoginConnectionString',
-      'dbName',
-      'dbGqlRole',
-      'dbLogin',
-      'dbLoginPassword',
-      'dbOwner',
-      'dbOwnerPassword',
-      'pgHost',
-      'pgPort',
-      'pgUserSuffix',
-      'pgSSLMode',
-      'pgRoot',
-      'pgRootPassword',
-      'dbLocalizationReplicationSlot',
-    ),
-  );
+  const config = getValidatedConfig(getBasicDbConfigDefinitions());
   const settings = await getMigrationSettings(config);
   await reset(settings, true);
 }
