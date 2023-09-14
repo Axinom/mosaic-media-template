b791d6dee4b93b7c6e514dcff00346037a84d96c
Sergey Trusov <trusov@axinom.com>
2023-09-14 09:59:28 +0300

    feat: ensureReplicationSlotAndPublicationExist helper used [AB#41392]

--- services/media/service/src/index.ts
+++ services/media/service/src/index.ts
@@ -1,6 +1,7 @@
 import {
   createLogicalReplicationService,
   CreatePostgresPoolConnectivityMetric,
+  ensureReplicationSlotAndPublicationExist,
   getLoginPgPool,
   setupLoginPgPool,
   setupOwnerPgPool,
@@ -80,8 +81,6 @@ async function bootstrap(): Promise<void> {
 
   // Enable multipart request support for GQL to support file upload.
   app.use(graphqlUploadExpress());
-  // Run database migrations to the latest committed state.
-  await applyMigrations(config);
 
   // Register shutdown actions. These actions will be performed on service shutdown; in the order of registration.
   const shutdownActions = setupShutdownActions(app, logger);
@@ -99,6 +98,33 @@ async function bootstrap(): Promise<void> {
     logger,
     shutdownActions,
   );
+
+  // Run database migrations to the latest committed state.
+  await applyMigrations(config);
+  if (config.isLocalizationEnabled) {
+    // Make sure logical replication slot and publication already exist or create them.
+    await ensureReplicationSlotAndPublicationExist({
+      replicationPgPool: ownerPool,
+      replicationSlotName: config.dbLocalizationReplicationSlot,
+      publicationName: PG_LOCALIZATION_PUBLICATION,
+      tableNames: [
+        'movies',
+        'movies_images',
+        'movie_genres',
+        'tvshows',
+        'tvshows_images',
+        'tvshow_genres',
+        'seasons',
+        'seasons_images',
+        'episodes',
+        'episodes_images',
+        'collections',
+        'collections_images',
+      ],
+      schemaName: 'app_public',
+      logger,
+    });
+  }
 
   // Sync defined permissions to the ID service.
   await syncPermissions(config, logger);
