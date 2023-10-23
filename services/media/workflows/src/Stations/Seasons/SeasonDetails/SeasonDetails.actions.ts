e832dedf07d121cca9a64b9da8e0440a96d33ffd
Benjamin Schwendner <schwendner@axinom.de>
2023-10-23 14:32:41 +0200

    chore: bump integration lib and apply renaming

--- services/media/workflows/src/Stations/Seasons/SeasonDetails/SeasonDetails.actions.ts
+++ services/media/workflows/src/Stations/Seasons/SeasonDetails/SeasonDetails.actions.ts
@@ -1,4 +1,4 @@
-import { getTranslationEntryPoint } from '@axinom/mosaic-managed-workflow-integration';
+import { getLocalizationEntryPoint } from '@axinom/mosaic-managed-workflow-integration';
 import { FormActionData, IconName } from '@axinom/mosaic-ui';
 import { useHistory } from 'react-router';
 import { client } from '../../../apolloClient';
@@ -13,7 +13,7 @@ export function useSeasonDetailsActions(id: number): {
   readonly actions: FormActionData<SeasonDetailsFormData>[];
 } {
   const history = useHistory();
-  const localizationPath = getTranslationEntryPoint('season');
+  const localizationPath = getLocalizationEntryPoint('season');
 
   const [deleteSeasonMutation] = useDeleteSeasonMutation({
     client,
