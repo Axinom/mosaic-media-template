b8e8d1c17948640e3d1c08db07e698046623b53c
Sergey Trusov <trusov@axinom.com>
2023-11-07 11:06:08 +0100

    feat: max pool connections increased from 10 to 25 [AB#41558]

--- services/entitlement/service/src/graphql/postgraphile-middleware.ts
+++ services/entitlement/service/src/graphql/postgraphile-middleware.ts
@@ -1,4 +1,4 @@
-import { getOwnerPgPool } from '@axinom/mosaic-db-common';
+import { getLoginPgPool, getOwnerPgPool } from '@axinom/mosaic-db-common';
 import { AuthenticationConfig } from '@axinom/mosaic-id-guard';
 import { Express } from 'express';
 import { postgraphile } from 'postgraphile';
@@ -11,13 +11,10 @@ export const setupPostGraphile = async (
   authConfig: AuthenticationConfig,
 ): Promise<void> => {
   const ownerPool = getOwnerPgPool(app);
+  const loginPool = getLoginPgPool(app);
   const options = buildPostgraphileOptions(config, ownerPool, authConfig);
 
-  const middleware = postgraphile(
-    config.dbLoginConnectionString,
-    'app_public',
-    options,
-  );
+  const middleware = postgraphile(loginPool, 'app_public', options);
 
   // The req.ip and req.ips values are populated based on the socket address and X-Forwarded-For header, starting at the first untrusted address.
   // This must be aligned with deployed environment configuration.
