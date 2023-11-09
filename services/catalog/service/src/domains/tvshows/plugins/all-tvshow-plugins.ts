764fbeb42ee388b0e6c0452d392fd3ef53e118bb
Sergey Trusov <117624390+AxTrusov@users.noreply.github.com>
2023-11-09 13:02:25 +0100

    feat: localizable properties are embedded into the root entity using views (#193)
    [AB#41880]

--- services/catalog/service/src/domains/tvshows/plugins/all-tvshow-plugins.ts
+++ services/catalog/service/src/domains/tvshows/plugins/all-tvshow-plugins.ts
@@ -1,11 +1,6 @@
 import { makePluginByCombiningPlugins } from 'graphile-utils';
-import { EntityLocalizationPlugin } from '../../../graphql/plugins';
 import { ExtendEpisodeQueryWithCountryCodePlugin } from './extend-episode-query-with-country-code-plugin';
 
 export const AllTvshowPlugins = makePluginByCombiningPlugins(
   ExtendEpisodeQueryWithCountryCodePlugin,
-  EntityLocalizationPlugin('tvshow', 'tvshow_localizations'),
-  EntityLocalizationPlugin('tvshow_genre', 'tvshow_genre_localizations'),
-  EntityLocalizationPlugin('season', 'season_localizations'),
-  EntityLocalizationPlugin('episode', 'episode_localizations'),
 );
