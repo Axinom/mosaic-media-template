492fd8a65f27f3e738d02685efdfed8daed3f7e9
Sergey Trusov <117624390+AxTrusov@users.noreply.github.com>
2023-11-21 10:45:18 +0100

    feat: catalog service whitelists custom locale header (#206)
    [AB#42148]

--- services/catalog/service/src/graphql/postgraphile-options.ts
+++ services/catalog/service/src/graphql/postgraphile-options.ts
@@ -33,7 +33,7 @@ import {
 export function buildPostgraphileOptions(
   config: Config,
 ): PostGraphileOptions<Request, Response> {
-  return new PostgraphileOptionsBuilder()
+  let options = new PostgraphileOptionsBuilder()
     .setDefaultSettings(config.isDev, config.graphqlGuiEnabled)
     .setErrorsHandler((errors, req) => {
       return enhanceGraphqlErrors(
@@ -43,7 +43,6 @@ export function buildPostgraphileOptions(
         logGraphQlError(catalogLogMapper),
       );
     })
-    .setHeader('Access-Control-Max-Age', 86400)
     .setPgSettings(async (req) => ({
       role: config.dbGqlRole,
       [MOSAIC_LOCALE_PG_KEY]: req.headers[MOSAIC_LOCALE_HEADER_KEY],
@@ -60,6 +59,40 @@ export function buildPostgraphileOptions(
       AllCollectionPlugins,
       AddErrorCodesEnumPluginFactory([MosaicErrors, CommonErrors]),
     )
-    .addGraphileBuildOptions({ pgSkipInstallingWatchFixtures: true })
-    .build();
+    .addGraphileBuildOptions({ pgSkipInstallingWatchFixtures: true });
+
+  options = setCustomCorsHeaders(options);
+  return options.build();
 }
+
+/**
+ * Used to set custom CORS headers since the standard PostGraphile CORS headers which are added
+ * via the { `enableCors`: true } property in the buildPostgraphileOptions are not customizable
+ */
+const setCustomCorsHeaders = (
+  options: PostgraphileOptionsBuilder,
+): PostgraphileOptionsBuilder => {
+  return options
+    .setProperties({ enableCors: false })
+    .setHeader(
+      'Access-Control-Allow-Headers',
+      [
+        'Origin',
+        'X-Requested-With',
+        'Accept',
+        'Authorization',
+        'X-Apollo-Tracing',
+        'Content-Type',
+        'Content-Length',
+        'X-PostGraphile-Explain',
+        MOSAIC_LOCALE_HEADER_KEY, // allow the custom header used
+      ].join(', '),
+    )
+    .setHeader(
+      'Access-Control-Allow-Methods',
+      ['HEAD', 'GET', 'POST'].join(', '),
+    )
+    .setHeader('Access-Control-Allow-Origin', '*')
+    .setHeader('Access-Control-Expose-Headers', 'X-GraphQL-Event-Stream')
+    .setHeader('Access-Control-Max-Age', 86400);
+};
