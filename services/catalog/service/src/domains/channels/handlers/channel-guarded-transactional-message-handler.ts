e070caa0bd1bbbd5cf16d5137d165d3abc098851
Frank Zehelein <zehelein@axinom.com>
2024-03-01 14:27:47 +0100

    feat: inline auth for ChannelGuardedTransactionalMessageHandler

--- services/catalog/service/src/domains/channels/handlers/channel-guarded-transactional-message-handler.ts
+++ services/catalog/service/src/domains/channels/handlers/channel-guarded-transactional-message-handler.ts
@@ -3,26 +3,19 @@ import {
   setPgSettingsConfig,
 } from '@axinom/mosaic-db-common';
 import {
-  AuthenticatedManagementSubject,
   getAuthenticatedManagementSubject,
-  GuardedConfig,
   GuardedContext,
-  GuardedTransactionalInboxMessageHandler,
   IdGuardErrors,
-  permissionsCheck,
 } from '@axinom/mosaic-id-guard';
 import { MessagingSettings } from '@axinom/mosaic-message-bus-abstractions';
 import { Logger, MosaicError } from '@axinom/mosaic-service-common';
-import { TypedTransactionalMessage } from '@axinom/mosaic-transactional-inbox-outbox';
+import {
+  TransactionalInboxMessageHandler,
+  TypedTransactionalMessage,
+} from '@axinom/mosaic-transactional-inbox-outbox';
 import { DatabaseClient } from 'pg-transactional-outbox';
 import { Config } from '../../../common';
 
-export type MediaGuardedConfig = GuardedConfig & {
-  tenantId: string;
-  environmentId: string;
-};
-
-const channelPublishPermissions = ['ADMIN', 'CHANNELS_EDIT'];
 const channelServiceId = 'ax-channel-service';
 
 /**
@@ -30,45 +23,41 @@ const channelServiceId = 'ax-channel-service';
  */
 export abstract class ChannelGuardedTransactionalMessageHandler<
   T,
-> extends GuardedTransactionalInboxMessageHandler<T, Config> {
+> extends TransactionalInboxMessageHandler<T, Config> {
   constructor(
     messagingSettings: MessagingSettings,
     logger: Logger,
     config: Config,
   ) {
-    super(messagingSettings, channelPublishPermissions, logger, config, {
-      tenantId: config.tenantId,
-      environmentId: config.environmentId,
-      authEndpoint: config.idServiceAuthBaseUrl,
-    });
-  }
+    const authWrapper = async <TMessage>(
+      message: TypedTransactionalMessage<TMessage>,
+      dbClient: DatabaseClient,
+    ): Promise<GuardedContext> => {
+      const token = message.metadata.authToken;
+      if (token === undefined) {
+        throw new MosaicError(IdGuardErrors.AccessTokenRequired);
+      }
+      const subject = await getAuthenticatedManagementSubject(token, {
+        tenantId: this.config.tenantId,
+        environmentId: this.config.environmentId,
+        authEndpoint: this.config.idServiceAuthBaseUrl,
+      });
 
-  /**
-   * Override the authentication to check the permissions from the Channel Service
-   */
-  protected override authenticateAndAuthorize = async <TMessage>(
-    message: TypedTransactionalMessage<TMessage>,
-    dbClient: DatabaseClient,
-  ): Promise<GuardedContext> => {
-    const token = message.metadata.authToken;
-    if (token === undefined) {
-      throw new MosaicError(IdGuardErrors.AccessTokenRequired);
-    }
-    const subject = await getAuthenticatedManagementSubject(token, {
-      tenantId: this.config.tenantId,
-      environmentId: this.config.environmentId,
-      authEndpoint: this.config.idServiceAuthBaseUrl,
-    });
-    await permissionsCheck(subject, this.permissions, channelServiceId);
-    this.setPgSettings(dbClient, subject);
-    return { subject };
-  };
+      const subjectPermissions = subject.permissions?.[channelServiceId];
+      if (
+        subjectPermissions === undefined ||
+        !Array.isArray(subjectPermissions)
+      ) {
+        throw new MosaicError({
+          code: IdGuardErrors.Unauthorized.code,
+          message: `Permission check failed as the subject has no permissions for the Channel Service.`,
+        });
+      }
 
-  protected override async setPgSettings(
-    ownerClient: DatabaseClient,
-    subject: AuthenticatedManagementSubject,
-  ): Promise<void> {
-    const pgSettings = buildAuthPgSettings(subject, channelServiceId);
-    await setPgSettingsConfig(pgSettings, ownerClient);
+      const pgSettings = buildAuthPgSettings(subject, channelServiceId);
+      await setPgSettingsConfig(pgSettings, dbClient);
+      return { subject };
+    };
+    super(messagingSettings, logger, config, authWrapper);
   }
 }
