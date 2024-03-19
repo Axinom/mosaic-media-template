4964e4dabbdfd994279feba06ca4088ae3b2a36e
Frank Zehelein <zehelein@axinom.com>
2024-03-19 13:24:03 +0100

    test: test fixes and improvements

--- services/media/service/src/common/utils/is-localization-available.spec.ts
+++ services/media/service/src/common/utils/is-localization-available.spec.ts
@@ -1,3 +1,4 @@
+import { getAuthenticatedManagementSubject } from '@axinom/mosaic-id-guard';
 import { Logger } from '@axinom/mosaic-service-common';
 import { Config } from '../config';
 import { updateConfigWithActualLocalizationAvailability } from './is-localization-available';
@@ -5,6 +6,10 @@ import { requestServiceAccountToken } from './token-utils';
 
 jest.mock('./token-utils', () => ({
   requestServiceAccountToken: jest.fn(),
+}));
+
+jest.mock('@axinom/mosaic-id-guard', () => ({
+  getAuthenticatedManagementSubject: jest.fn().mockReturnValue({}),
 }));
 
 describe('localizationAvailableCheck', () => {
@@ -35,42 +40,12 @@ describe('localizationAvailableCheck', () => {
     expect(logger.warn).not.toHaveBeenCalled();
   });
 
-  it('should keep isLocalizationEnabled as true if the service account token has localization permission', async () => {
-    // Arrange - fake signed token contains permissions for the localization service
-    (requestServiceAccountToken as jest.Mock).mockResolvedValue(
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwZXJtaXNzaW9ucyI6eyJheC1sb2NhbGl6YXRpb24tc2VydmljZSI6WyJFTlRJVFlfREVGSU5JVElPTlNfRURJVCJdfSwiaWF0IjoxNzEwNzcyNTk4fQ.z6WfHLgtpsDr_dFQpaULMZGPzSAhvB8g2AUZ-nmqCk8',
-    );
-
-    // Act
-    await updateConfigWithActualLocalizationAvailability(mockConfig, logger);
-
-    // Assert
-    expect(requestServiceAccountToken).toHaveBeenCalledWith(mockConfig);
-    expect(mockConfig.isLocalizationEnabled).toBe(true);
-    expect(logger.warn).not.toHaveBeenCalled();
-  });
-
-  it('should set isLocalizationEnabled to false if the service account token does not have localization permission', async () => {
-    // Arrange - fake signed token does not contain permissions for the localization service
-    const mockToken =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwZXJtaXNzaW9ucyI6e30sImlhdCI6MTcxMDc3MjYzMX0.FUgks4EvGilaqdTUvzo3eyU5NqV5vt9aZP63vo3XJck';
-    (requestServiceAccountToken as jest.Mock).mockResolvedValue(mockToken);
-
-    // Act
-    await updateConfigWithActualLocalizationAvailability(mockConfig, logger);
-
-    // Assert
-    expect(requestServiceAccountToken).toHaveBeenCalledWith(mockConfig);
-    expect(mockConfig.isLocalizationEnabled).toBe(false);
-    expect(logger.warn).not.toHaveBeenCalled();
-  });
-
-  it('should keep isLocalizationEnabled true if an error is thrown', async () => {
+  it('should keep isLocalizationEnabled as true if the service account has localization permission', async () => {
     // Arrange
-    const mockError = new Error('Token parsing error');
-    (requestServiceAccountToken as jest.Mock).mockResolvedValue('in.val.id');
-    jest.spyOn(JSON, 'parse').mockImplementationOnce(() => {
-      throw mockError;
+    (getAuthenticatedManagementSubject as jest.Mock).mockResolvedValue({
+      permissions: {
+        'ax-localization-service': ['ENTITY_DEFINITIONS_EDIT'],
+      },
     });
 
     // Act
@@ -79,6 +54,43 @@ describe('localizationAvailableCheck', () => {
     // Assert
     expect(requestServiceAccountToken).toHaveBeenCalledWith(mockConfig);
     expect(mockConfig.isLocalizationEnabled).toBe(true);
-    expect(logger.warn).toHaveBeenCalled();
+    expect(logger.warn).not.toHaveBeenCalled();
+  });
+
+  it('should set isLocalizationEnabled to false if the service account does not have localization permission', async () => {
+    // Arrange
+    (getAuthenticatedManagementSubject as jest.Mock).mockResolvedValue({
+      permissions: {
+        'ax-other-service': ['SOME_PERMISSION'],
+      },
+    });
+    // Act
+    await updateConfigWithActualLocalizationAvailability(mockConfig, logger);
+
+    // Assert
+    expect(requestServiceAccountToken).toHaveBeenCalledWith(mockConfig);
+    expect(mockConfig.isLocalizationEnabled).toBe(false);
+    expect(logger.warn).toHaveBeenCalledWith(
+      'The configuration value states that localization should be enabled but the service account does not have permissions for the localization service. Disabling localizations until the Media Service is restarted and the checks are run again.',
+    );
+  });
+
+  it('should keep isLocalizationEnabled true if an error is thrown', async () => {
+    // Arrange
+    const mockError = new Error('Token parsing error');
+    (getAuthenticatedManagementSubject as jest.Mock).mockRejectedValue(
+      mockError,
+    );
+
+    // Act
+    await updateConfigWithActualLocalizationAvailability(mockConfig, logger);
+
+    // Assert
+    expect(requestServiceAccountToken).toHaveBeenCalledWith(mockConfig);
+    expect(mockConfig.isLocalizationEnabled).toBe(true);
+    expect(logger.warn).toHaveBeenCalledWith(
+      mockError,
+      'Could not get the service account token to check if the localization service is enabled.',
+    );
   });
 });
