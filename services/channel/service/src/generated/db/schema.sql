ab5837bf87bfcb6c7d1f84fbbf2f13d6fd19c21e
Frank Zehelein <zehelein@axinom.com>
2024-07-10 14:59:53 +0200

    feat: set the publishing user for channels and playlists

--- services/channel/service/src/generated/db/schema.sql
+++ services/channel/service/src/generated/db/schema.sql
@@ -226,6 +226,21 @@ $_$;
 
 
 --
+-- Name: define_publish_trigger(text, text); Type: FUNCTION; Schema: app_hidden; Owner: -
+--
+
+CREATE FUNCTION app_hidden.define_publish_trigger(tablename text, schemaname text) RETURNS void
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+  EXECUTE 'DROP TRIGGER IF EXISTS _900__publish_user ON ' || schemaName || '.' || tableName || ';';
+  EXECUTE 'CREATE trigger _900__publish_user BEFORE INSERT OR UPDATE ON ' || schemaName || '.' || tableName ||
+          ' FOR EACH ROW EXECUTE PROCEDURE app_hidden.tg__publish_audit_fields();';
+END;
+$$;
+
+
+--
 -- Name: define_publish_trigger(text, text, text); Type: FUNCTION; Schema: app_hidden; Owner: -
 --
 
@@ -3642,14 +3657,14 @@ CREATE TRIGGER _500_gql_programs_updated AFTER UPDATE ON app_public.programs FOR
 -- Name: channels _900__publish_user; Type: TRIGGER; Schema: app_public; Owner: -
 --
 
-CREATE TRIGGER _900__publish_user BEFORE INSERT OR UPDATE OF title, description, placeholder_video_id ON app_public.channels FOR EACH ROW EXECUTE FUNCTION app_hidden.tg__publish_audit_fields();
+CREATE TRIGGER _900__publish_user BEFORE INSERT OR UPDATE ON app_public.channels FOR EACH ROW EXECUTE FUNCTION app_hidden.tg__publish_audit_fields();
 
 
 --
 -- Name: playlists _900__publish_user; Type: TRIGGER; Schema: app_public; Owner: -
 --
 
-CREATE TRIGGER _900__publish_user BEFORE INSERT OR UPDATE OF title, start_date_time, calculated_duration_in_seconds, channel_id ON app_public.playlists FOR EACH ROW EXECUTE FUNCTION app_hidden.tg__publish_audit_fields();
+CREATE TRIGGER _900__publish_user BEFORE INSERT OR UPDATE ON app_public.playlists FOR EACH ROW EXECUTE FUNCTION app_hidden.tg__publish_audit_fields();
 
 
 --
@@ -3958,6 +3973,14 @@ GRANT USAGE ON SCHEMA public TO channel_service_gql_role;
 
 REVOKE ALL ON FUNCTION app_hidden.create_localizable_entity_triggers(aggregateid text, tablename text, entitytype text, localizable_fields text, required_fields text) FROM PUBLIC;
 GRANT ALL ON FUNCTION app_hidden.create_localizable_entity_triggers(aggregateid text, tablename text, entitytype text, localizable_fields text, required_fields text) TO channel_service_gql_role;
+
+
+--
+-- Name: FUNCTION define_publish_trigger(tablename text, schemaname text); Type: ACL; Schema: app_hidden; Owner: -
+--
+
+REVOKE ALL ON FUNCTION app_hidden.define_publish_trigger(tablename text, schemaname text) FROM PUBLIC;
+GRANT ALL ON FUNCTION app_hidden.define_publish_trigger(tablename text, schemaname text) TO channel_service_gql_role;
 
 
 --
