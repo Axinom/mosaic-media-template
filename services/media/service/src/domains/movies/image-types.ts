0425194caf61b698f2637673d75b812459781a41
Madawa Illesinghe <illesinghe@axinom.com>
2024-07-17 10:12:15 +0530

    adding new image types to media - image service

--- services/media/service/src/domains/movies/image-types.ts
+++ services/media/service/src/domains/movies/image-types.ts
@@ -1,4 +1,9 @@
 export const movieImageTypes = [
-  { image_type: 'movie_cover', title: 'Movie Cover' },
-  { image_type: 'movie_teaser', title: 'Movie Teaser' },
+  { image_type: 'movie_cover_1x1', title: 'Movie Cover 1x1' },
+  { image_type: 'movie_cover_16x9', title: 'Movie Cover 16x9' },
+
+  { image_type: 'movie_clean_cover_1x1', title: 'Movie Clean Cover 1x1' },
+  { image_type: 'movie_clean_cover_16x9', title: 'Movie Clean Cover 16x9' },
+
+  { image_type: 'movie_list_9x13', title: 'Movie List 9x13' },
 ];
--- services/media/service/src/domains/tvshows/image-types.ts
+++ services/media/service/src/domains/tvshows/image-types.ts
@@ -1,8 +1,31 @@
 export const tvshowImageTypes = [
-  { image_type: 'tvshow_cover', title: 'TV Show Cover' },
-  { image_type: 'tvshow_teaser', title: 'TV Show Teaser' },
-  { image_type: 'season_cover', title: 'Season Cover' },
-  { image_type: 'season_teaser', title: 'Season Teaser' },
-  { image_type: 'episode_cover', title: 'Episode Cover' },
-  { image_type: 'episode_teaser', title: 'Episode Teaser' },
+  { image_type: 'tvshow_cover_1x1', title: 'TV Show Cover 1x1' },
+  { image_type: 'tvshow_cover_16x9', title: 'TV Show Cover 16x9' },
+
+  { image_type: 'tvshow_clean_cover_1x1', title: 'TV Show Clean Cover 1x1' },
+  {
+    image_type: 'tvshow_clean_cover_16x9',
+    title: 'TV Show Clean Cover 16x9',
+  },
+  { image_type: 'tvshow_list_9x13', title: 'TV Show List 9x13' },
+
+  { image_type: 'season_cover_1x1', title: 'Season Cover 1x1' },
+  { image_type: 'season_cover_16x9', title: 'Season Cover 16x9' },
+
+  { image_type: 'season_clean_cover_1x1', title: 'Season Clean Cover 1x1' },
+  {
+    image_type: 'season_clean_cover_16x9',
+    title: 'Season Clean Cover 16x9',
+  },
+  { image_type: 'season_list_9x13', title: 'Season List 9x13' },
+
+  { image_type: 'episode_cover_1x1', title: 'Episode Cover 1x1' },
+  { image_type: 'episode_cover_16x9', title: 'Episode Cover 16x9' },
+
+  { image_type: 'episode_clean_cover_1x1', title: 'Episode Clean Cover 1x1' },
+  {
+    image_type: 'episode_clean_cover_16x9',
+    title: 'Episode Clean Cover 16x9',
+  },
+  { image_type: 'episode_list_9x13', title: 'Episode List 9x13' },
 ];
