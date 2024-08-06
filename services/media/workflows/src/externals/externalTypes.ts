d27cb3e7e6e8ecaffb8fdad9411aca3e3cdb1d3a
Isuru Jayasooriya <jayasooriya@axinom.com>
2024-08-06 11:04:21 +0530

    fix: typings for fast registration

--- services/media/workflows/src/externals/externalTypes.ts
+++ services/media/workflows/src/externals/externalTypes.ts
@@ -1,4 +1,27 @@
+import { ID, Maybe } from '@axinom/mosaic-managed-workflow-integration';
 import '@axinom/mosaic-portal';
+
+interface FastProviderData {
+  type: string;
+  label: string;
+  selectionComponent: React.FC<{
+    onClose: () => void;
+    onSelected: (items: ProgramEntity[]) => void;
+  }>;
+  detailsResolver?: (params: {
+    entityId: string;
+    entityType: string;
+  }) => string;
+}
+
+type FastProviderType = 'fast-provider';
+
+interface ProgramEntity {
+  title: string;
+  videoId: ID;
+  entityId: string;
+  imageId?: Maybe<ID>;
+}
 declare module '@axinom/mosaic-portal' {
   /**
    * Route resolver registration function.
@@ -10,5 +33,12 @@ declare module '@axinom/mosaic-portal' {
         dynamicRouteSegments?: Record<string, string> | string,
       ) => void,
     ): string | undefined;
+  }
+
+  interface ProviderRegistration {
+    (type: FastProviderType, data: FastProviderData): void;
+  }
+  interface GetProviders {
+    (type: FastProviderType): FastProviderData[];
   }
 }
