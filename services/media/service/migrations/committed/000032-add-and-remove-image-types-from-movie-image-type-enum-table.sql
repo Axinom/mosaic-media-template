fe82eecbfcb5b522d52cc22ccc3d2cf681074862
Kaushalya Sandamali <sandamali@axinom.com>
2024-10-15 14:09:43 +0000

    Merged PR 13107: migration issue fixed.
    migration issue fixed.

--- services/media/service/migrations/committed/000032-add-and-remove-image-types-from-movie-image-type-enum-table.sql
+++ services/media/service/migrations/committed/000032-add-and-remove-image-types-from-movie-image-type-enum-table.sql
@@ -1,28 +1,8 @@
 --! Previous: sha1:97414ab5b38903fb0a0d6698a39f4a8b20d552f7
---! Hash: sha1:13f9b84db5b847abcd5ac1cc57382947277c474f
+--! Hash: sha1:349d101530551c485cc80f98f48cce4e40ceec29
 --! Message: add-and-remove-image-types-from-movie-image-type-enum-table
 
 -- movie_image_type enum
-UPDATE app_public.movies_images SET image_type = 'COVER_1X1' WHERE image_type = 'COVER';
-DELETE FROM app_public.movie_image_type WHERE value = 'COVER';
-
-UPDATE app_public.movies_images SET image_type = 'COVER_1X1' WHERE image_type = 'TEASER';
-DELETE FROM app_public.movie_image_type WHERE value = 'TEASER';
-
-UPDATE app_public.movies_images SET image_type = 'COVER_1X1' WHERE image_type = 'MOVIE_COVER_1x1';
-DELETE FROM app_public.movie_image_type WHERE value = 'MOVIE_COVER_1x1';
-
-UPDATE app_public.movies_images SET image_type = 'COVER_16X9' WHERE image_type = 'MOVIE_COVER_16x9';
-DELETE FROM app_public.movie_image_type WHERE value = 'MOVIE_COVER_16x9';
-
-UPDATE app_public.movies_images SET image_type = 'CLEAN_COVER_1X1' WHERE image_type = 'MOVIE_CLEAN_COVER_1x1';
-DELETE FROM app_public.movie_image_type WHERE value = 'MOVIE_CLEAN_COVER_1x1';
-
-UPDATE app_public.movies_images SET image_type = 'CLEAN_COVER_16X9' WHERE image_type = 'MOVIE_CLEAN_COVER_16x9';
-DELETE FROM app_public.movie_image_type WHERE value = 'MOVIE_CLEAN_COVER_16x9';
-
-UPDATE app_public.movies_images SET image_type = 'LIST_9X13' WHERE image_type = 'MOVIE_LIST_9x13';
-DELETE FROM app_public.movie_image_type WHERE value = 'MOVIE_LIST_9x13';
 
 INSERT INTO app_public.movie_image_type (value, description)
 VALUES 
@@ -32,3 +12,10 @@ VALUES
     ('CLEAN_COVER_16x9', 'Clean Cover 16x9'),
     ('LIST_9x13', 'List 9x13')
 ON CONFLICT (value) DO NOTHING;
+
+
+UPDATE app_public.movies_images SET image_type = 'COVER_1X1' WHERE image_type = 'COVER';
+DELETE FROM app_public.movie_image_type WHERE value = 'COVER';
+
+UPDATE app_public.movies_images SET image_type = 'COVER_1X1' WHERE image_type = 'TEASER';
+DELETE FROM app_public.movie_image_type WHERE value = 'TEASER';
