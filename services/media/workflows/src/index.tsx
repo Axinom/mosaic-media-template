59d608985a84b0d3007d4b788c034f43321f62d1
Yasith Ariyasena <yasith.ariyasena@gmail.com>
2024-10-17 13:48:52 +0530

    fix: Fix incorrect import

--- services/media/workflows/src/index.tsx
+++ services/media/workflows/src/index.tsx
@@ -6,7 +6,7 @@ import { bindExtensions } from './externals/piralExtensions';
 import './global.scss';
 import { MediaIconName, MediaIcons } from './MediaIcons';
 import { initializeConfig, piletConfig } from './piletConfig';
-import { register as registerFASTProviders } from './providers/registrations';
+
 import { sortTiles } from './sortTiles/sortTiles';
 import { register as registerCollections } from './Stations/Collections/registrations';
 import { register as registerEpisodes } from './Stations/Episodes/registrations';
@@ -60,5 +60,4 @@ export function setup(app: PiletApi): void {
   registerIngest(app, extensions);
   registerSnapshotRegistry(app, extensions);
   registerCollections(app, extensions);
-  registerFASTProviders(app);
 }
