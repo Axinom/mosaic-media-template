9d98ae13c05108d6a35cb2cd02be360029a458cd
Madawa Illesinghe <illesinghe@axinom.com>
2024-11-06 02:58:53 +0530

    adding new plugin for custom filter.

---
+++ services/catalog/service/src/graphql/plugins/custom-filters-plugin.ts
@@ -0,0 +1,21 @@
+import type { Plugin } from 'graphile-build';
+import type { SQL } from 'graphile-build-pg';
+import type { Build } from 'postgraphile';
+
+export const CustomFiltersPlugin: Plugin = (builder) => {
+  builder.hook('build', (build: Build) => {
+    const { pgSql: sql, connectionFilterArrayOperators } = build as Build & {
+      connectionFilterArrayOperators: Record<string, Record<string, any>>;
+    };
+
+    // Add a new filter operator that checks if the column contains the specified values
+    connectionFilterArrayOperators.iContains = {
+      description:
+        'Does the column contain the specified values? (case-insensitive)',
+      resolve: (identifier: SQL, value: SQL): SQL =>
+        sql.fragment`ARRAY(SELECT LOWER(unnest(${identifier}::text[]))) @> ARRAY(SELECT LOWER(unnest(${value}::text[])))`,
+    };
+
+    return build;
+  });
+};
