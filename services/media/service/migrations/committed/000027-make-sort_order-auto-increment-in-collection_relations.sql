62aa3d515bc5fca901e6dac81dc0d098dfa490be
axinom-geethaka <pitigala@axinom.com>
2025-08-28 17:05:42 +0530

    feat: fixed syntax in trigger creation and added for update for row locking

--- services/media/service/migrations/committed/000027-make-sort_order-auto-increment-in-collection_relations.sql
+++ services/media/service/migrations/committed/000027-make-sort_order-auto-increment-in-collection_relations.sql
@@ -1,18 +1,29 @@
 --! Previous: sha1:b74cde38f51e59d36d33923a186339559759f757
---! Hash: sha1:25f0c9f515c338068eb854ead5e0c71af210d6e5
+--! Hash: sha1:e941d1e38e76fad134ba61f00361c4c83ddb6500
 --! Message: Make sort_order auto-increment in collection_relations
 
 -- create or replace the trigger to set the sort order to MAX + 1 for that specific collection if no value provided for sort_order
 CREATE OR REPLACE FUNCTION app_public.set_sort_order()
 RETURNS TRIGGER AS $$
+DECLARE
+    max_sort_order INTEGER;
 BEGIN
     IF NEW.sort_order IS NULL THEN
-        NEW.sort_order := COALESCE((SELECT MAX(sort_order) FROM app_public.collection_relations WHERE collection_id = NEW.collection_id), 0) + 1;
+        SELECT COALESCE(MAX(sort_order), 0)
+        INTO max_sort_order
+        FROM
+        (
+          SELECT sort_order FROM app_public.collection_relations
+          WHERE collection_id = NEW.collection_id FOR UPDATE
+        );
+        NEW.sort_order := max_sort_order + 1;
     END IF;
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE TRIGGER set_sort_order_trigger
+DROP TRIGGER IF EXISTS set_sort_order_trigger ON app_public.collection_relations;
+
+CREATE TRIGGER set_sort_order_trigger
 BEFORE INSERT ON app_public.collection_relations
 FOR EACH ROW EXECUTE FUNCTION app_public.set_sort_order();
