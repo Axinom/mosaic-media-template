8e68aa21e260a7e3ed354f2d6b9dc4e6b7a8a2b6
Inga Eglite <eglite@axinom.com>
2025-09-15 11:52:18 +0200

    fix: rename container class to severityRendererContainer in SeverityRenderer

--- services/channel/workflows/src/util/Publishing/SeverityRenderer/SeverityRenderer.tsx
+++ services/channel/workflows/src/util/Publishing/SeverityRenderer/SeverityRenderer.tsx
@@ -19,7 +19,7 @@ export const SeverityRenderer: ColumnRenderer<Data> = (val): ReactNode => {
     }
 
     return (
-      <div className={classes.container}>
+      <div className={classes.severityRendererContainer}>
         {image}
         <div className={classes.text}>{formatTitleCase(val as string)}</div>
       </div>
