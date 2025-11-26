87a1651a74d23fa3f8a5662d5a12e6c848bf6bd2
mihiranga <mihiranga@axinom.com>
2025-11-26 08:00:06 +0530

    chore: add descriptive audio for catalogue service video stream type

--- services/catalog/service/migrations/current.sql
+++ services/catalog/service/migrations/current.sql
@@ -1,3 +1,13 @@
---! Message: replace-with-migration-name
+--! Message: add-descriptive-audio-stream-type
 
--- Remove this comment line and write your migration here. Make sure to keep one empty line between 'Message' header and first migration line to properly name future migration file.
+DO $$ 
+BEGIN
+  IF NOT EXISTS (
+    SELECT 1 
+    FROM app_public.video_stream_type 
+    WHERE value = 'DESCRIPTIVE_AUDIO'
+  ) THEN
+    INSERT INTO app_public.video_stream_type (value, description)
+    VALUES ('DESCRIPTIVE_AUDIO', 'Descriptive Audio');
+  END IF;
+END $$;
